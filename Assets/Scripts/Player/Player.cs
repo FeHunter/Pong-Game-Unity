75616c9c7c693255c6621913bffcbcdb8077f336
Felipe Hunter <hunterfe1111@gmail.com>
2023-02-16 12:58:34 -0300

    Start to create a player control script

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float _posY;
+    [field: SerializeField] public float MinY { get; set; }
+    [field: SerializeField] public float MaxY { get; set; }
+    [field: SerializeField] public float Speed { get; set; }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector2(transform.position.x, Mathf.Clamp(_posY, MinY, MaxY));
+
+        if (Keyboard.current.wKey.isPressed || Keyboard.current.upArrowKey.isPressed)
+        {
+            _posY += Speed * Time.deltaTime;
+        }
+        else if (Keyboard.current.sKey.isPressed || Keyboard.current.downArrowKey.isPressed)
+        {
+            _posY -= Speed * Time.deltaTime;
+        }
     }
 }
